2f6639fa978df4f6b4011aad31c67ebf0e1cc1cf
Oleg Shchurov <o.shchurov@firecode-team.ru>
2026-03-05 23:20:50 +0300

    feat: init shadcn ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.css";
+import { ThemeProvider } from "#shared/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,11 +30,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ru">
+    <html lang="ru" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 import { getProjects } from "#server/services";
+import { ModeToggle } from "#shared/components/mode-toggle";
+import { Button } from "#ui";
 
 export default async function Home() {
   const projects = await getProjects();
   console.log("loaded", projects);
-  return <div>Initial Page</div>;
+  return (
+    <div>
+      <p>Initial Page</p>
+      <Button variant={"default"}>add</Button>
+      <ModeToggle />
+    </div>
+  );
 }
