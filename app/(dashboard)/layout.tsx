65430fade70137ce4d27c89da2b998dac343b7bf
Oleg Shchurov <o.shchurov@firecode-team.ru>
2026-03-16 19:20:44 +0300

    feat: added some styled divs to the layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,9 +3,13 @@ import { PropsWithChildren } from "react";
 
 export default async function DashboardLayout({ children }: PropsWithChildren) {
   return (
-    <div className="px-6 py-4">
-      <Header />
-      <div className="mt-8">{children}</div>
+    <div className="min-h-screen">
+      <div className="mx-auto min-h-screen w-full max-w-7xl">
+        <div className="px-6 py-4">
+          <Header />
+          <main className="mt-6">{children}</main>
+        </div>
+      </div>
     </div>
   );
 }
