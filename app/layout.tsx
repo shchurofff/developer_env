b0a9b31221f961ed6412a564febfaa8adcf83a66
Oleg Shchurov <o.shchurov@firecode-team.ru>
2026-03-20 14:53:03 +0300

    feat: added toaster provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
 import "../styles/globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "#ui";
 
 const jetbrains = JetBrains_Mono({
   subsets: ["latin"],
@@ -49,6 +50,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Toaster />
       </body>
     </html>
   );
