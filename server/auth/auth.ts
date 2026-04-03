32eb7d032d644e1ff75b3609a2550a6eaba2fcca
Oleg Shchurov <o.shchurov@firecode-team.ru>
2026-04-03 09:51:24 +0300

    feat: added base url to the better auth

--- server/auth/auth.ts
+++ server/auth/auth.ts
@@ -4,6 +4,7 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "#server/db/db";
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
